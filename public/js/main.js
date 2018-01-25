2b4dd41f5b0e8d7fb43a225a7c2c06a11d553765
Bruno M. Ribas <brunoribas68@gmail.com>
2018-01-25 21:59:45 -0200

    pasta public

---
+++ public/js/main.js
@@ -0,0 +1,82 @@
+  var TempoInicial = $('#tempoDigitacao').text();
+  var campo = $('.campoDeDigitacao');
+  var mensagemInicial = $(".msg").text();
+  var mensagem = $(".msg");
+  var frase = $(".frase").text();
+  console.log(frase);
+
+
+  $("document").ready(function(){
+    InicializaFrase();
+    InicializaContadores();
+    InicializaCronometro();
+    $('.reset').click(reiniciaJogo);
+  });
+
+
+  function InicializaFrase(){
+  var numeroPalavras = frase.split(" ").length;
+  var tamanhoFrase = $("#tamanhoFrase");
+
+  tamanhoFrase.text(numeroPalavras);
+  }
+
+
+  function  InicializaContadores(){
+    campo.on("input",function(){
+      var conteudo = campo.val();
+      var qtdPalavra = conteudo.split(/\S+/).length - 1;
+      $(".contadorDePalavras").text(qtdPalavra);
+      var qtdCaracteres = conteudo.length;
+      $(".contadorDeCaracteres").text(qtdCaracteres);
+    })
+  }
+
+
+  function InicializaCronometro(){
+    campo.one("focus", function(){
+      var tempoDigitacao = $("#tempoDigitacao").text();
+      var idInterval =  setInterval(function(){
+        tempoDigitacao--;
+        $("#tempoDigitacao").text(tempoDigitacao);
+        if(tempoDigitacao == 0){
+          campo.attr("disabled",true);
+          clearInterval(idInterval);
+          campo.toggleClass("campoDisabled");
+          mensagem.addClass("msgDisabled");
+          mensagem.text("GAME OVER");
+          campo.addClass("bordaVermelha");
+        }
+      },1000);
+    });
+  }
+
+  function reiniciaJogo(){
+    campo.attr("disabled",false);
+    campo.val("");
+    mensagem.text(mensagemInicial);
+    $(".contadorDePalavras").text("0");
+    $(".contadorDeCaracteres").text("0");
+    $('#tempoDigitacao').text(TempoInicial);
+    campo.toggleClass("campoDisabled");
+    mensagem.removeClass("msgDisabled");
+    campo.removeClass("bordaVerde");
+    campo.removeClass("bordaVermelha");
+    campo.addClass("bordaPreta");
+    InicializaCronometro();
+  }
+
+  campo.on("input",function(){
+    campo.removeClass("bordaPreta");
+    var digitado = campo.val();
+    var comparavel = frase.substr(0,digitado.length)
+    console.log("Digitado: " + digitado);
+    console.log("Comparavel :" + comparavel);
+    if(digitado == comparavel){
+    campo.addClass("bordaVerde");
+    campo.removeClass("bordaVermelha");
+    }else{
+      campo.addClass("bordaVermelha");
+      campo.removeClass("bordaVerde");
+    }
+  });
